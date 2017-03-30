8afb6ff4508a8a2a42156a69e25d9cfca91bd573
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2017-03-29 20:02:06 -0700

    Add next to route

--- index.js
+++ index.js
@@ -5,30 +5,33 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const session = require('express-session')({
     secret: 'secret',
-    resave: true,
+    resave: false,
     saveUninitialized: true
 });
 var sharedsession = require('express-socket.io-session');
 
 app.use(session);
 
-io.use(sharedsession(session, {
-    autoSave: true
-}))
+io.use(sharedsession(session))
 
 io.on('connection', (socket) => {
 
     socket.handshake.session.data = ['connection']
+    socket.handshake.session.save();
     console.log(socket.handshake.session);
 
     socket.on('login', data => {
         socket.handshake.session.data.push('login');
+        socket.handshake.session.save();
         console.log(socket.handshake.session);
+
     });
 })
 
 app.get('/route', (req, res, next) => {
-    console.log(req.session.data);
+    req.session.data ? req.session.data.push('route') : req.session.data = ['route']
+    next();
+    console.log(req.session);
 })
 
 app.get('/*', (req, res, next) => {
